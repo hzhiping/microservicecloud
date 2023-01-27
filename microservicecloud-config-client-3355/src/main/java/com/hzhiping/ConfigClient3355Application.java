50cee5a5c4fa49dfce298e9f12547df7275e0add
hzhiping <23955637@qq.com>
2023-01-27 13:11:05 +0800

    feature:添加springcloud config客户端端配置代码

---
+++ microservicecloud-config-client-3355/src/main/java/com/hzhiping/ConfigClient3355Application.java
@@ -0,0 +1,15 @@
+package com.hzhiping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author hzhiping
+ * @date 2023/01/27
+ */
+@SpringBootApplication
+public class ConfigClient3355Application {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClient3355Application.class, args);
+    }
+}
